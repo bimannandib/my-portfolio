b65238daa5e37fea2670bd0181359b338feac323
Biman Nandi <bimannandib@gmail.com>
2026-02-15 23:41:15 +0530

    nav-bar created

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header>
+            <nav className="nav-bar">
+                <h1>BN</h1>
+                <ul className="navbar-links">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
