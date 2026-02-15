e45a2b9a98ec81ea991a868f5775557543915204
Biman Nandi <bimannandib@gmail.com>
2026-02-16 04:40:42 +0530

    hero-section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Components/Header'
+import HeroSection from "./Components/HeroSection";
 
 function App() {
 
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   )
 }
---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,23 @@
+import profile from "../Assets/profile.png"
+export default function HeroSection() {
+    return (
+      <section className="hero-section">
+        <div className="hero-texts">
+          <h1>Hi, I'm Biman Nandi</h1>
+          <h6>Web Developer | Engineering Student | Problem Solver</h6>
+          <p>
+            Pre-final-year engineering student passionate about building
+            interactive web applications with React and solving complex problems
+            with Data Structures & Algorithms.
+          </p>
+          <div className="button-section">
+            <button className="btn1">View My Work</button>
+            <button className="btn2">Get In Touch</button>
+          </div>
+        </div>
+        <div className="hero-img">
+            <img src={profile} alt="profile-pic" />
+        </div>
+      </section>
+    );
+}
