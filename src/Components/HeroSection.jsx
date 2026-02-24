e5202e8585c1b85db7ec00868126cf9bd2c6128c
Biman Nandi <bimannandib@gmail.com>
2026-02-25 00:38:52 +0530

    made header-section more reuse

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,14 +1,12 @@
-import profile from "../Assets/profile.png"
+import Data from '../Data/HeroSectionData'
 export default function HeroSection(props) {
     return (
       <section className="hero-section">
         <div className="hero-texts">
-          <h1>Hi, I'm Biman Nandi</h1>
-          <h6>Web Developer | Engineering Student | Problem Solver</h6>
+          <h1>Hi, I'm {Data.name}</h1>
+          <h6>{Data.title}</h6>
           <p>
-            Pre-final year engineering student passionate about building
-            interactive web applications with React and solving complex problems
-            with Data Structures & Algorithms.
+            {Data.description}
           </p>
           <div className="button-section">
             <button onClick={props.onProjectClick} className="btn1">View My Work</button>
@@ -16,7 +14,7 @@ export default function HeroSection(props) {
           </div>
         </div>
         <div className="hero-img">
-            <img src={profile} alt="profile-pic" />
+            <img src={Data.profile} alt="profile-pic" />
         </div>
       </section>
     );
---
+++ src/Data/HeroSectionData.js
@@ -0,0 +1,9 @@
+import Profile from '../Assets/profile.png'
+const heroData = {
+  name: "Biman Nandi",
+  title: "Frontend Developer (React) | Engineering Student | Problem Solver",
+  description:
+    "Pre-final year engineering student with hands-on experience building responsive, user-friendly web interfaces using React, HTML, CSS, and JavaScript. Strong foundation in Data Structures & Algorithms, with a growing interest in backend development. Passionate about creating clean UI and smooth user experiences.",
+  profile: Profile,
+};
+export default heroData
